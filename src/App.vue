c872925a578ee750293751c4a481ec3e72f8fd3b
Ollie <hello@ollie.zone>
2021-06-13 09:12:02 -0500

    Add footer!

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
   <div class="mx-auto min-w-min">
-    <header>
-      <img class="w-96 mx-auto" src="/logo.png" />
-    </header>
+    <div class="min-h-screen">
+      <header>
+        <img class="w-96 mx-auto" src="/logo.png" />
+      </header>
 
-    <main class="mx-auto w-min space-y-4 py-4">
-      <div class="mx-auto w-min h-min">
-        <Preview />
-      </div>
+      <main class="mx-auto w-min space-y-4 py-4">
+        <div class="mx-auto w-min h-min">
+          <Preview />
+        </div>
 
-      <div class="mx-auto flex content-center w-max space-x-1 my-4">
-        <ToggleCssButton />
+        <div class="mx-auto flex content-center w-max space-x-1 my-4">
+          <ToggleCssButton />
 
-        <CopyCssToClipboardButton />
+          <CopyCssToClipboardButton />
 
-        <ToggleGlobalSettingsButton />
-      </div>
+          <ToggleGlobalSettingsButton />
+        </div>
 
-      <GlobalSettings v-show="showGlobalSettings" />
+        <GlobalSettings v-show="showGlobalSettings" />
 
-      <Avatars />
-    </main>
+        <Avatars />
+      </main>
+    </div>
+
+    <footer
+      class="my-8 opacity-40 hover:opacity-100 transition w-full leading-normal"
+    >
+      <a href="https://www.github.com/olliepup" target="_blank">
+        <img
+          class="rounded-full border-purple-700 border w-32 mx-auto mb-2"
+          src="/icon.jpg"
+        />
+      </a>
+      <p class="text-center mx-auto">
+        An
+        <a href="https://www.github.com/olliepup" target="_blank">Oliver</a>
+        production. Ollie portrait by
+        <a href="https://www.twitter.com/tinyaliem" target="_blank">tinyaliem</a
+        >, example avatars by
+        <a href="https://www.twitter.com/slimedrops" target="_blank"
+          >slimedrops</a
+        >.
+      </p>
+      <p class="text-center mx-auto font-bold">
+        <a href="https://www.github.com/olliepup" target="_blank">Feedback?</a>
+      </p>
+    </footer>
   </div>
 </template>
 
